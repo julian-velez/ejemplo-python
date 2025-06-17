be087bb26fda1207e882cc3b4db0157f012a921b
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-06-17 10:11:02 -0500

    ejemplo-python
    ejemplo-python

---
+++ ejemplo/numero.py
@@ -0,0 +1,14 @@
+# Definimos una variable llamada numero_1 que contiene el texto (cadena de caracteres) "350"
+numero_1 = "350"
+
+# Definimos una variable llamada numero_2 que contiene el texto (cadena de caracteres) "150"
+numero_2 = "150"
+
+# Usamos la función int() para convertir las cadenas de texto en números enteros
+# Luego sumamos esos dos números y mostramos el resultado en pantalla con print()
+print(int(numero_1) + int(numero_2))
+# Lo que pasa aquí es:
+# int("350") se convierte en 350 (entero)
+# int("150") se convierte en 150 (entero)
+# 350 + 150 = 500
+# El print muestra: 500
